3e43fffe275b9d1ae58ba2c08e1b16307133818d
Manuel Saavedra <manuel.saavedra@orexplore.com>
2025-11-19 18:11:27 +0800

    Extended agents.py skeleton

--- src/ABMOS/agents.py
+++ src/ABMOS/agents.py
@@ -1,6 +1,8 @@
+import numpy as np
 from __future__ import annotations
 from typing import Dict, Optional, Union, Any
-from collections.abc import Iterable
+from collections.abc import Callable, Hashable, Iterable, Iterator, MutableSet, Sequence
+from random import Random
 
 
 class Agent:
@@ -21,6 +23,12 @@ class Agent:
         """
         self.__dict__[name] = value
 
+    def step(self):
+        """
+        Step the individual agent object
+        """
+        pass
+
     def update_state(self):
         """
         Updates the internal state of the agent after the model has stepped.
@@ -35,3 +43,43 @@ class Agent:
         :param neighbours: A list of all agents that "neighbour" this agent in any model layer.
         """
         pass
+    
+    
+class AgentSet(MutableSet, Sequence):
+    """
+    An ordered collection of Agent objects that maintains consistency for the Model
+    """
+    def __init__(self, agents: Iterable[Agent], random: Random | None = None):
+        self._agents = None
+        pass
+    
+    def __len__(self) -> int:
+        """
+        :return: the number of agents present in the AgentSet
+        """
+        return len(self._agents)
+    
+    def __iter__(self) -> Iterator[Agent]:
+        """
+        :return: an iterator which yields each agent in the AgentSet 
+        """
+        return self._agents.keys()
+    
+    def __contains__(self, agent: Agent) -> bool:
+        """
+        :param agent: the specific Agent object to check for
+        :return: a boolean indicating if the specified Agent object is in the AgentSet
+        """
+        return agent in self._agents
+    
+    def select(self, filter_func: Callable[[Agent], bool] | None = None, inplace: bool = False, k: int = np.inf) -> AgentSet:
+        """
+        Select a subset of Agent objects from the AgentSet.
+        
+        :param filter_func: a function used to filter the Agent objects
+        :param inplace: if True, modify the existing AgentSet, otherwise return a new AgentSet
+        :param k: the maximum number of Agent objects to include in the subset
+        :return: an AgentSet containing a filtered subset of Agents
+        """
+        pass
+    
