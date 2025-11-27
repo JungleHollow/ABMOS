e8e9ce7b63c68ea3c2416e8df8ca8e2992363530
Manuel Saavedra <manuel.saavedra@orexplore.com>
2025-11-28 07:53:24 +0800

    Expanded the logging.py skeleton

--- src/ABMOS/logging.py
+++ src/ABMOS/logging.py
@@ -4,5 +4,14 @@ class ABMOSLogger:
     both during and after simulation
     """
 
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self, verbose: bool = False, print_interval: int = 10, write_file: bool = True
+    ) -> None:
+        """
+        :param verbose: a flag to indicate if extended information should be printed during logging
+        :param print_interval: the number of model iterations to run in between each printed logging output
+        :param write_file: a flag to indicate if a log file should be written to disk at the end of logging
+        """
+        self.verbose = verbose
+        self.print_interval = print_interval
+        self.write_file = write_file
