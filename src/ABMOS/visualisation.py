729ed222c7bf0c347b1a94c7a1359d99574eeddc
Manuel Saavedra <manuel.saavedra@orexplore.com>
2025-12-05 16:14:22 -0300

    Initialised the visualisation.py skeleton

--- src/ABMOS/visualisation.py
+++ src/ABMOS/visualisation.py
@@ -1,8 +1,20 @@
-"""
-The visualisations will include:
-    - Graph structure plotting
-    - Realtime model runtime display
-    - Post-runtime summarisation graphs
-    - Per-agent lifetime information
-    - Per-graph lifetime information
-"""
+from __future__ import annotations
+
+import numpy as np
+import rustworkx as rx
+from matplotlib import pyplot as plt
+from rustworkx.visualization import mpl_draw
+
+
+class ABVisualiser:
+    """
+    The visualisations will include:
+        - Graph structure plotting
+        - Realtime model runtime display
+        - Post-runtime summarisation graphs
+        - Per-agent lifetime information
+        - Per-graph lifetime information
+    """
+
+    def __init__(self):
+        self.fig, self.ax = plt.subplots()
